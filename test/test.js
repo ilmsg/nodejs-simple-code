68ab401d1f114ed2994f3b0ae27e9c288013e56e
eak netpanya <eak.netpanya@gmail.com>
2018-03-06 15:36:24 +0700

    update

--- test/test.js
+++ test/test.js
@@ -9,10 +9,10 @@ const app = require('../app.js');
 const server = supertest(app);
 
 describe("SAMPLE unit test", function(){
-  it("should return home page",function(done){
+  it("should return home page", function(done){
     server
     .get("/")
-    .expect("Content-type",/text/)
+    .expect("Content-type", /text/)
     .expect(200)
     .end(function(err,res){
       res.status.should.equal(200);
